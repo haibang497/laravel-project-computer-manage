c38039114c73380d2b820be73c9486ec7801393f
Nguyễn Hải Bằng <44358203+haibang497@users.noreply.github.com>
2020-12-05 20:27:18 +0700

    Update Order.php
    Change

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,13 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    use HasFactory;
     protected $fillable=['title', 'dayCreate', 'status', 'user_id'];
 
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
 
     public function computers()
     {
